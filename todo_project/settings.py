b7ce6eab2e66dd5b66ed27be4c858ea0343db8aa
ananthanandanan <ananthanandanan@gmail.com>
2020-11-04 08:55:39 +0530

    delete secret

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')1#hh0(@i+an@$33ud=6gr^8@mjgovp9nds*0=-!$^csg4=#+-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
