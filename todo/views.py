3f3ef42d0688865267ace7c81f51ecb4b0138cc0
ananthanandanan <ananthanandanan@gmail.com>
2020-11-05 18:26:30 +0530

    completed

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Todo
 from django.utils import timezone
+from django.http import  HttpResponseRedirect
 
 # Create your views here.
 
 
 def home(request):
+    
     
     return render(request, 'base.html')
 
@@ -18,11 +20,21 @@ def add_todo(request):
     #adding to db
     Todo.objects.create(text = todo_item,add_date = add_time)
     #get the Todo object
-    todo_items = Todo.objects.all().order_by('-add_date')
-    print(todo_items)    
+    todo_items = Todo.objects.all().order_by('-add_date')    
     
     stuff_for_frontend = {
         
         'todo_items' : todo_items
     }
     return render(request, 'todo/add_todo.html', stuff_for_frontend)    
+
+def delete_todo(request, todo_id):
+    
+    print(todo_id)
+    Todo.objects.get(id=todo_id).delete()
+    todo_items = Todo.objects.all().order_by('-add_date')
+    
+    frontend_stuff = {
+        'todo_items' : todo_items
+    }
+    return HttpResponseRedirect('/')
